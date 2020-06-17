2987aa6056617e4eb6eb59b2dd646a2ced60b155
john katua <johnkatua99@gmail.com>
2020-06-17 08:56:07 +0300

    initial commit

---
+++ src/actions/secret.js
@@ -0,0 +1,4 @@
+const username = 'johnkatua';
+const password = '@mwashokatua';
+
+export {username,password};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
+import App from './components/App';
 
-ReactDOM.render(
-  <React.StrictMode>
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import rootReducer from './reducers';
+import thunk from 'redux-thunk';
+
+import {fetchMemes} from './actions';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+store.subscribe(() => console.log('store', store.getState()));
+store.dispatch(fetchMemes());
+
+
+ReactDom.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  </Provider>
+  ,document.getElementById('root'));
